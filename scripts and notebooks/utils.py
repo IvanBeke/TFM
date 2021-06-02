947d338ee382998c2cca1ecbf9cc164af7ef1f1d
Iván Iglesias <ivan.beke96@gmail.com>
2021-06-02 13:57:00 +0200

    Guardar estado ejecución y resultado final #5

--- scripts and notebooks/utils.py
+++ scripts and notebooks/utils.py
@@ -1,5 +1,5 @@
-import json, requests, time
-
+import json, requests, time, os
+import dill as pickle
 
 def build_url(region: str, base_url: str, endpoint: str) -> str:
     """Build complete url to call.
@@ -47,6 +47,19 @@ def load_json(file: str) -> dict:
 
 
 def call_with_retry(url: str, headers: dict, params: dict = {}, retries: int = 1) -> dict:
+    """Call the Riot API with retries in case the request fails
+
+    Parameters
+    ----------
+    url : str
+    headers : dict
+    params : dict, optional by default {}
+    retries : int, optional by default 1
+
+    Returns
+    -------
+    dict
+    """
     response = requests.get(url, headers=headers, params=params)
     if response.status_code == 200:
         response = response.json()
@@ -57,3 +70,35 @@ def call_with_retry(url: str, headers: dict, params: dict = {}, retries: int = 1
         time.sleep(retry_after)
         response = call_with_retry(url, headers, params, retries+1)
         return response
+
+
+def save_execution_state(object, path: str):
+    """Serialize an object
+
+    Parameters
+    ----------
+    object : Any
+    path : str
+    """
+    with open(path, 'wb') as f:
+        pickle.dump(object, f)
+
+
+def load_execution_state(path: str, default):
+    """Load a serialized object, return default if it doesn't exist
+
+    Parameters
+    ----------
+    path : str
+    default : Any
+
+    Returns
+    -------
+    Any
+    """
+    if os.path.exists(path) and os.stat(path).st_size != 0:
+        with open(path, 'rb') as f:
+            object = pickle.load(f)
+    else:
+        object = default
+    return object
