58c23a655e1e172b22220fdd92e14818c29e6069
Iván Iglesias <ivan.beke96@gmail.com>
2021-06-02 02:25:17 +0200

    Obtención de partidas a partir de jugadores #5

--- scripts and notebooks/utils.py
+++ scripts and notebooks/utils.py
@@ -1,4 +1,4 @@
-import json
+import json, requests, time
 
 
 def build_url(region: str, base_url: str, endpoint: str) -> str:
@@ -17,6 +17,7 @@ def build_url(region: str, base_url: str, endpoint: str) -> str:
     """
     return f'https://{region}.{base_url}{endpoint}'
 
+
 def save_json(obj: dict, file: str) -> None:
     """Save dict as json file.
 
@@ -27,6 +28,7 @@ def save_json(obj: dict, file: str) -> None:
     """
     with open(f'outputs/{file}', 'w+') as f:
         json.dump(obj, f, indent=4)
+
 
 def load_json(file: str) -> dict:
     """Load a json file and return its dict.
@@ -42,3 +44,16 @@ def load_json(file: str) -> dict:
     with open(file, 'r') as f:
         obj = json.load(f)
     return obj
+
+
+def call_with_retry(url: str, headers: dict, params: dict = {}, retries: int = 1) -> dict:
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        response = response.json()
+        return response
+    else:
+        print(response.text)
+        retry_after = int(response.headers['Retry-After'])
+        time.sleep(retry_after)
+        response = call_with_retry(url, headers, params, retries+1)
+        return response
