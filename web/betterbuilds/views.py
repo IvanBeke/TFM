6c1853a9f696c3c352f2c357d73779761ba7dd97
Iván Iglesias <ivan.beke96@gmail.com>
2021-07-18 16:49:40 +0200

    Alerta si no existen objetos #24
    Closes #24

--- web/betterbuilds/views.py
+++ web/betterbuilds/views.py
@@ -16,12 +16,13 @@ def champions_builds(request: HttpRequest, champion_id: int) -> HttpResponse:
     champion = cass.Champion(id=champion_id, locale='es_ES')
     frequent_build = FrequentBuilds.objects.filter(champion=champion_id, type='basic').first()
     items = {}
-    for build in frequent_build.builds:
-        build['support'] = round(build['support'], 2) * 100
-        build['items'] = list()
-        for item in build['itemsets']:
-            build['items'].append(cass.Item(id=item))
-        build['items'] = sorted(build['items'], key=lambda item:-item.gold.total)
+    if frequent_build is not None:
+        for build in frequent_build.builds:
+            build['support'] = round(build['support'], 2) * 100
+            build['items'] = list()
+            for item in build['itemsets']:
+                build['items'].append(cass.Item(id=item))
+            build['items'] = sorted(build['items'], key=lambda item:-item.gold.total)
     return render(request, 'betterbuilds/champion_builds.html', {
         'champion': champion,
         'frequent_build': frequent_build,
