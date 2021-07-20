d0b6f4f42c04eee05037d2cb6b98aefefd7edfe1
Iván Iglesias <ivan.beke96@gmail.com>
2021-07-20 12:20:26 +0200

    Imagen, descripción y consejos de campeón #22
    Closes #22

--- web/betterbuilds/views.py
+++ web/betterbuilds/views.py
@@ -15,16 +15,14 @@ def champions(request: HttpRequest) -> HttpResponse:
 def champions_builds(request: HttpRequest, champion_id: int) -> HttpResponse:
     champion = cass.Champion(id=champion_id, locale='es_ES')
     frequent_build = FrequentBuilds.objects.filter(champion=champion_id, type='basic').first()
-    items = {}
     if frequent_build is not None:
         for build in frequent_build.builds:
             build['support'] = round(build['support'], 2) * 100
             build['items'] = list()
             for item in build['itemsets']:
-                build['items'].append(cass.Item(id=item))
+                build['items'].append(cass.Item(id=item, locale='es_ES'))
             build['items'] = sorted(build['items'], key=lambda item:-item.gold.total)
     return render(request, 'betterbuilds/champion_builds.html', {
         'champion': champion,
         'frequent_build': frequent_build,
-        'items': items,
     })
