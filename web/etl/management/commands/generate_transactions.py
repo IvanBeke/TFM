1aba7ec52d8318992ad36fdaab456a0b7112663a
Iván Iglesias <ivan.beke96@gmail.com>
2021-07-26 23:34:56 +0200

    Filtrado de items #19

--- web/etl/management/commands/generate_transactions.py
+++ web/etl/management/commands/generate_transactions.py
@@ -2,6 +2,7 @@ from typing import Any, Optional
 from django.core.management.base import BaseCommand
 from pymongo import MongoClient
 from web.utils import *
+from django_cassiopeia import cassiopeia as cass
 import time
 
 class Command(BaseCommand):
@@ -9,6 +10,11 @@ class Command(BaseCommand):
 
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
         start = time.time()
+        items = cass.Items()
+        exclude_items = [0]
+        for item in items:
+            if 'Boots' in item.tags or len(item.builds_from) == 0:
+                exclude_items.append(item.id)
 
         client = MongoClient()
         client.etl.matches.aggregate([
@@ -16,7 +22,8 @@ class Command(BaseCommand):
                 '$unwind': {
                     'path': '$participants'
                 }
-            }, {
+            },
+            {
                 '$set': {
                     'items': [
                         '$participants.stats.item0',
@@ -24,11 +31,27 @@ class Command(BaseCommand):
                         '$participants.stats.item2',
                         '$participants.stats.item3',
                         '$participants.stats.item4',
-                        '$participants.stats.item5',
-                        '$participants.stats.item6'
+                        '$participants.stats.item5'
                     ]
                 }
-            }, {
+            },
+            {
+                '$project': {
+                    'participants.championId': 1,
+                    'items': {
+                        '$filter': {
+                            'input': '$items',
+                            'as': 'item',
+                            'cond': {
+                                '$not': {
+                                    '$in': ['$$item', exclude_items]
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            {
                 '$group': {
                     '_id': '$participants.championId',
                     'championId': {
@@ -38,10 +61,11 @@ class Command(BaseCommand):
                         '$push': '$items'
                     }
                 }
-            }, {
+            },
+            {
                 '$out': 'transactions'
             }
-        ])
+        ], allowDiskUse=True)
 
         end = time.time()
         print(f'\nEl proceso ha tardado {get_time(end - start)}')
